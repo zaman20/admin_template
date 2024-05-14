8c1da427e38d3a55bb48af68930575eea0fed82a
zaman20 <adorzaman18@gmail.com>
2024-05-14 06:28:50 +0600

    basic page design done

---
+++ inc/js/myscripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  
+  //mobile-menu
+  $('.menu-area ul li').on('click',function(e){
+  
+
+    $(this).children('ul').slideToggle();
+    $(this).children('span').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+
+  //delete Btn
+  $('.dlt-btn').on('click',function(){
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  });
+
+
+  //image preview 
+  $('.za-fimg-box ').hide();
+
+$('#fImage').on('change',function(event){
+  $('.za-fimg-box ').show();
+  let image = URL.createObjectURL(event.target.files[0]);
+  $('#feature_img').attr('src',image);
+});
+//image preview hide
+$('.za-removeimg').on('click',function(){
+  $('.za-fimg-box ').hide();
+  $('#fImage').val('');
+});
+
+});
